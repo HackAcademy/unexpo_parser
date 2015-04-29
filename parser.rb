3ee7a00da9bd503f26948475925815f272326757
jfuentes <jfuentes@4geeks.co@4geeks.co>
2015-04-29 21:41:32 +0000

    creating internal class parsers

--- parser.rb
+++ parser.rb
@@ -7,7 +7,16 @@ class String
   end
 
   def string_from_marker marker1
-    self[/#{Regexp.escape(marker1)}(.*?)/m, 1]
+    self[/#{Regexp.escape(marker1)}(.*?)/m, self.length]
+  end
+
+  def splitparse_by_markers *markers
+    regexpstring = '\^['
+    markers.each do |m|
+      regexpstring << "#{Regexp.escape(m)}" 
+    end
+    regexpstring << ']{1}'
+    self.split(/#{regexpstring}/)
   end
 end
 
@@ -16,11 +25,31 @@ class Category
 end
 
 class Book
-  attr_accessor :title, :subtitle, :category, :author, :editorial
+  attr_accessor :title, :subtitle, :categories, :authors, :editorial, :callnumber, :isbn, :pubtype, :dimensions
 
   def parse_title hash_title
     @title = hash_title.string_between_markers '^a','^b'
     @subtitle = hash_title.string_from_marker '^b'
+  end
+
+  def parse_call hash_call
+    @callnumber = hash_call.string_from_marker '^a'
+  end
+
+  def parse_editorial hash_call
+    @callnumber = hash_call.string_from_marker '^a'
+  end
+
+  def parse_isbn hash_isbn
+    @isbn = hash_isbn.splitparse_by_markers 'a', 'b'
+    @dimensions.shift
+    @dimensions = @dimensions.join(', ')
+  end
+
+  def parse_dimensions hash_dimensions
+    @dimensions = hash_dimensions.splitparse_by_markers('a', 'b', 'c')
+    @dimensions.shift
+    @dimensions = @dimensions.join(', ')
   end
 end
 
@@ -75,26 +104,38 @@ def main
 
   parser.library_hash.each do |hash_book|
     book = Book.new
+    book.authors = []
+    book.categories= []
     hash_book.each do |key,value|
       case key
         when '650'
           category = Category.new
           category.name = value
-          book.category = category
-        when '100'
+          book.categories.push category
+        when '100', '700'
           author = Author.new
           author.name = value
-          book.author = author
+          book.authors.push author
         when '245'
           book.parse_title value
         when '260'
           book.editorial = value
+        when '82'
+          book.parse_call value
+        when '20'
+          book.parse_isbn value
+        when '842'
+          book.pubtype = value 
+        when '300'
+          book.parse_dimensions value
       end
     end
     library.books.push book
   end
 
-  puts library.books[0].subtitle
+  # puts parser.library_hash[0].inspect
+  
+  puts library.books[0].dimensions
   #library.save
 end
 
